b4a39184dc63a135a364da9818c9c56e38454d52
Fernando Contreras <fercreek@gmail.com>
2017-03-13 02:22:36 +0100

    Using calendar-component

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,11 +22,80 @@ function ($scope, $stateParams, $http) {
 
 }])
    
-.controller('calendarCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('calendarCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $http) {
+	var url = "http://localhost:8000/records/";
 
+	activate();
+
+
+	function activate(){
+		$scope.calendar = {};
+		$http.get(url)
+			.then(function(res){
+				console.log('res', res.data);
+				// $scope.calendar.eventSource = createRandomEvents();
+				$scope.calendar.eventSource = res.data;
+
+
+				res.data.forEach(function(data) {
+					console.log('data', data);
+					// data.eventType = 1;
+					if (!data.fecha) {
+						data.fecha = new Date();
+					}
+
+					data.title = data.nombre;
+					data.allDay = false;
+					data.startTime = data.fecha;
+					data.endTime = data.fecha;
+				});
+			});
+
+	}
+
+	$scope.onEventSelected = function (event) {
+	    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+	};
+
+    function createRandomEvents() {
+        var events = [];
+        for (var i = 0; i < 50; i += 1) {
+            var date = new Date();
+            var eventType = Math.floor(Math.random() * 2);
+            var startDay = Math.floor(Math.random() * 90) - 45;
+            var endDay = Math.floor(Math.random() * 2) + startDay;
+            var startTime;
+            var endTime;
+            if (eventType === 0) {
+                startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
+                if (endDay === startDay) {
+                    endDay += 1;
+                }
+                endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
+                events.push({
+                    title: 'All Day - ' + i,
+                    startTime: startTime,
+                    endTime: endTime,
+                    allDay: true
+                });
+            } else {
+                var startMinute = Math.floor(Math.random() * 24 * 60);
+                var endMinute = Math.floor(Math.random() * 180) + startMinute;
+                startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
+                endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
+                events.push({
+                    title: 'Event - ' + i,
+                    startTime: startTime,
+                    endTime: endTime,
+                    allDay: false
+                });
+            }
+        }
+        return events;
+    }
 
 }])
    
