5dda393c79cee621d7f5bbabbf31d0c76f3fa700
Fernando Contreras <fercreek@gmail.com>
2017-03-13 01:36:20 +0100

    POST API Record

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,24 @@
 angular.module('app.controllers', [])
   
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $http) {
 
+	var url = "http://localhost:8000/records/";
+	$scope.submit = submit;
+	$scope.record = {};
+
+	function submit() {
+		console.log('submit', $scope.record);
+		$http.post(url, $scope.record, {'Content-Type': 'application/json'})
+			.then(function(res){
+				console.log('res', res)
+			})
+			.catch(function(err){
+				console.log('err', err)
+			});
+	}
 
 }])
    
